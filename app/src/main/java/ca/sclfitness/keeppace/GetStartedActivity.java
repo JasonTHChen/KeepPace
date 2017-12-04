b6be03c7fc5a7287669a6f5ec8dd9611274ba446
Daniel <daniel_katz@hotmail.ca>
2017-12-03 19:53:17 -0800

    Added aligned pause/resume and reset buttons , made TOS hyperlink instead of popup , made About hyperlink (broken) ,

--- app/src/main/java/ca/sclfitness/keeppace/GetStartedActivity.java
+++ app/src/main/java/ca/sclfitness/keeppace/GetStartedActivity.java
@@ -3,7 +3,10 @@ package ca.sclfitness.keeppace;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class GetStartedActivity extends AppCompatActivity {
 
@@ -12,14 +15,8 @@ public class GetStartedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
 
-        View TOS = findViewById(R.id.getStarted);
-
-        TOS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(GetStartedActivity.this, Pop.class));
-            }
-        });
+        ((TextView) findViewById(R.id.getStarted)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView) findViewById(R.id.getStarted)).setText(Html.fromHtml(getResources().getString(R.string.TOS)));
     }
 
     public void onStart(View v) {
